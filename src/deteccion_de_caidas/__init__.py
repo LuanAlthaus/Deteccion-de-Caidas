73381d460e460e40bd39b74b9835d32be256ca61
LuanAlthaus <luanhenriquealthausback@gmail.com>
2026-08-28 23:28:22 -0300

    Agregue infos, besitos

--- src/deteccion_de_caidas/__init__.py
+++ src/deteccion_de_caidas/__init__.py
@@ -1,2 +1,9 @@
 def main() -> None:
     print("Hello from 1er-2do-semestre!")
+
+
+# no borrar
+
+#git add . // agregar todo a git
+#git commit -m "Escribir cambios"
+#git push origin master //subir a github
